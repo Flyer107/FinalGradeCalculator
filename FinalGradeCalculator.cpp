ba4581cff67697b43695404d41a51f5a13d859b8
Flyer107 <43021952+Flyer107@users.noreply.github.com>
2019-07-05 15:53:55 -0400

    Add project files.

---
+++ FinalGradeCalculator.cpp
@@ -0,0 +1,48 @@
+//FinalGrade.cpp - Calculates a students final grade in the class
+//Created by Kwesi Owubah on 07/05/2019
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//function prototype 
+double finalNeeded(double, double, double);
+
+int main()
+{
+	//declare variables
+	double currGrade;
+	double finWorth;
+	double gradeObjective;
+
+	double result = 0.0;
+
+	//get lengths of two sides
+	cout << "Current grade: ";
+	cin >> currGrade;
+	cout << "What percent is the final worth? ";
+	cin >> finWorth;
+	cout << "What grade (at least) do you want in the class? ";
+	cin >> gradeObjective;
+
+	//call function to calculate hypotenuse
+	result = finalNeeded(currGrade, finWorth, gradeObjective);
+
+	//display the length of the hypotenuse
+	cout << "The grade you need on your final to at least get a(n) " << gradeObjective << " is: "
+		<< result << endl;
+
+	system("pause");
+	return 0;
+}	//end of main function
+
+
+//*****function definition***** 
+double finalNeeded(double current, double worth, double objective) {
+	double val = 0.0;
+	double worthP = worth / 100;
+	val = (objective - ((1 - worthP) * current)) / worthP;
+	return val;
+}
+
+// F = ( G - ((1-w) x C) )/w
